6032f722dccdf4688fb629799d623b99e22270a4
Dominik Durner <durner@in.tum.de>
2016-11-11 20:01:34 +0100

    [parser] Added QueryParser Header File

--- include/QueryParser.hpp
+++ include/QueryParser.hpp
@@ -1,3 +1,48 @@
 #include "Database.hpp"
+#include <list>
+#include <string>
+
+namespace parser {
+
+struct Select {
+  std::string attribute;
+};
+
+struct Relation {
+  std::string tablename;
+  std::string binding;
+};
+
+struct Selection {
+  std::string binding;
+  std::string attribute;
+  std::string constant;
+};
+
+struct JoinSelection {
+  std::string binding_left;
+  std::string attribute_left;
+  std::string binding_right;
+  std::string attribute_right;
+};
 
 
+class QueryParser {
+public:
+  QueryParser(std::string database, std::string query);
+private:
+  void ParseSQLQuery();
+  void ParseSelects();
+  void ParseRelations();
+  void ParseSelection();
+  void ParseJoinSelection();
+
+  Database db_;
+  std::string query;
+  std::list<Select> selects_;
+  std::list<Relation> relations_;
+  std::list<Selection> selections_;
+  std::list<JoinSelection> join_selections_;
+};
+
+} // end ns
